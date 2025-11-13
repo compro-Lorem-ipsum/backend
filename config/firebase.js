4b16319640060bb1684775e1dd9b05cec4412aef
Azizah Salma <147806636+almaayunisaa@users.noreply.github.com>
2025-11-13 23:24:45 +0700

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,16 @@
 const admin = require("firebase-admin");
 
-// Ambil raw value dari Azure / GitHub Secrets
-let rawKey = process.env.FIREBASE_PRIVATE_KEY;
+// Private key dalam base64
+const base64Key = process.env.FIREBASE_PRIVATE_KEY_BASE64;
 
-rawKey = rawKey.replace(/\\n/g, '\n');
+// Decode Base64 → Buffer → String
+const decodedKey = Buffer.from(base64Key, "base64").toString("utf-8");
 
+// Buat service account object
 const serviceAccount = {
   project_id: process.env.FIREBASE_PROJECT_ID,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: rawKey,
+  private_key: decodedKey,
 };
 
 admin.initializeApp({
