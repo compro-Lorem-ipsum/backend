f19d3075815c96802521000f8f30a01a1c6ccfc4
Azizah Salma <147806636+almaayunisaa@users.noreply.github.com>
2025-11-13 22:54:07 +0700

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -3,21 +3,8 @@ const admin = require("firebase-admin");
 // Ambil raw value dari Azure / GitHub Secrets
 let rawKey = process.env.FIREBASE_PRIVATE_KEY;
 
-if (!rawKey) {
-  console.error("❌ FIREBASE_PRIVATE_KEY UNDEFINED at firebase.js load time");
-  rawKey = "";
-}
 
 rawKey = rawKey.replace(/\\n/g, '\n');
-
-// Jika BASE64 → decode ke PEM
-if (rawKey && !rawKey.includes("BEGIN PRIVATE KEY")) {
-  try {
-    rawKey = Buffer.from(rawKey, "base64").toString("utf8");
-  } catch (e) {
-    console.error("Gagal decode BASE64 private key:", e);
-  }
-}
 
 const serviceAccount = {
   project_id: process.env.FIREBASE_PROJECT_ID,
