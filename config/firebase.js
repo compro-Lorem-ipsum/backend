29829889aa95bd3b3ee587050940e92eadad02e1
Azizah Salma <147806636+almaayunisaa@users.noreply.github.com>
2025-11-13 21:51:44 +0700

    Update firebase.js
    update config

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,24 @@
 const admin = require("firebase-admin");
 
+// Decode private key BASE64 menjadi teks asli PEM
+const privateKey = Buffer.from(
+  process.env.FIREBASE_PRIVATE_KEY,
+  "base64"
+).toString("utf8");
+
+// Buat service account object
 const serviceAccount = {
   project_id: process.env.FIREBASE_PROJECT_ID,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: privateKey,
 };
 
+// Initialize firebase admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.firebasestorage.app`
+  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.firebasestorage.app`,
 });
 
+// Export bucket
 const bucket = admin.storage().bucket();
 module.exports = bucket;
